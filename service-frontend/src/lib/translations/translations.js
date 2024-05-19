9a90ff6871dc4df4750faa4cde2339c380e67817
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-05-19 18:48:20 +0900

    Make top page

---
+++ service-frontend/src/lib/translations/translations.js
@@ -0,0 +1,31 @@
+import i18n from 'sveltekit-i18n';
+
+/** @type {import('sveltekit-i18n').Config} */
+const config = {
+	loaders: [
+		{
+			locale: 'en',
+			key: 'common',
+			loader: async () => (await import('./common-en.json')).default
+		},
+		{
+			locale: 'ja',
+			key: 'common',
+			loader: async () => (await import('./common-ja.json')).default
+		},
+		{
+			locale: 'en',
+			key: 'index',
+			routes: ['/'],
+			loader: async () => (await import('./index/en.json')).default
+		},
+		{
+			locale: 'ja',
+			key: 'index',
+			routes: ['/'],
+			loader: async () => (await import('./index/ja.json')).default
+		}
+	]
+};
+
+export const { t, locale, locales, loading, loadTranslations } = new i18n(config);
--- service-frontend/src/routes/+page.svelte
+++ service-frontend/src/routes/+page.svelte
@@ -1,3 +1,27 @@
-<h1>ShortBook</h1>
-<p>有料記事投稿サービス</p>
-<p>2024年6月30日リリースまでお待ちください！</p>
+<script lang="ts">
+	import { Button, Textarea } from 'flowbite-svelte';
+	import { t } from '$lib/translations/translations';
+
+	let value = '';
+
+	function saveDraft() {
+		console.log(value);
+	}
+</script>
+
+<div class="h-screen bg-stone-50 px-4 py-8">
+	<div class="mx-auto flex max-w-4xl flex-col items-center">
+		<img src="/shortbook-logotype.svg" class="mb-16 w-3/5 max-w-96" alt={$t('common.logo_alt')} />
+		<p class="mb-4 text-3xl">{$t('index.catch_copy')}</p>
+		<Textarea
+			placeholder={$t('index.draft_placeholder')}
+			rows="12"
+			class="border-primary-700 mb-4 w-full"
+			bind:value
+		/>
+		<Button
+			class="to-primary-700 bg-gradient-to-b from-yellow-500 text-2xl"
+			on:click={() => saveDraft()}>{$t('index.save_label')}</Button
+		>
+	</div>
+</div>
