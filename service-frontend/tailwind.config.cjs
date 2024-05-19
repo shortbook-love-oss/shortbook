5447a10989e0b9e770ac6d8f0edc260bd5bff5a2
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-05-19 18:26:06 +0900

    Set primary color to ShortBook

--- service-frontend/tailwind.config.cjs
+++ service-frontend/tailwind.config.cjs
@@ -11,7 +11,22 @@ const config = {
 	darkMode: 'selector',
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: {
+					50: '#FCF9F9',
+					100: '#F4E7E7',
+					200: '#E8CBCA',
+					300: '#DCAFAE',
+					400: '#C98584',
+					500: '#BC6967',
+					600: '#AE4F4C',
+					700: '#924240',
+					800: '#592827',
+					900: '#2F1514'
+				}
+			}
+		}
 	}
 };
 
