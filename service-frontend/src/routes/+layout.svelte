f4257a11b50c88b68622bb1b757ee321c5c261e3
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-05-20 11:24:15 +0900

    Auto change html lang attr

--- service-frontend/src/routes/+layout.svelte
+++ service-frontend/src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const initLocale = navigator.language?.split('-')[0] || 'en';
+		document.documentElement.lang = initLocale;
+	});
 </script>
 
 <slot></slot>
