6723044b1704349d03aa5fbd550b90ad58feb253
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-10 10:51:25 +0900

    Support sign in with GitHub

--- service-frontend/src/auth.ts
+++ service-frontend/src/auth.ts
@@ -2,11 +2,12 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import LinkedIn from '@auth/sveltekit/providers/linkedin';
+import GitHub from '@auth/sveltekit/providers/github';
 
 const prisma = new PrismaClient();
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	trustHost: true,
 	adapter: PrismaAdapter(prisma),
-	providers: [LinkedIn]
+	providers: [LinkedIn, GitHub]
 });
