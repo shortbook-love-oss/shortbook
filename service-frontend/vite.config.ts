1ebff463d48eccbb83bede7b922052a0905f7d3e
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-11 15:37:33 +0900

    Install unplugin-icons and iconify

--- service-frontend/vite.config.ts
+++ service-frontend/vite.config.ts
@@ -2,16 +2,21 @@ import fs from 'fs';
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-	plugins: [paraglide({ project: './lib/i18n/project.inlang', outdir: './lib/i18n/paraglide' }), sveltekit()],
+	plugins: [
+		paraglide({ project: './lib/i18n/project.inlang', outdir: './lib/i18n/paraglide' }),
+		sveltekit(),
+		Icons({ compiler: 'svelte' })
+	],
 	server: {
 		host: true, // Access inside container
 		proxy: {},
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem')
-    }
+		https: {
+			key: fs.readFileSync('./localhost-key.pem'),
+			cert: fs.readFileSync('./localhost.pem')
+		}
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
