e709eb1eae0206a444481b7e4c2c434caf646ec8
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-11 16:52:07 +0900

    Put sidebar and screen layout centering

--- service-frontend/src/routes/+layout.svelte
+++ service-frontend/src/routes/+layout.svelte
@@ -1,12 +1,22 @@
 <script>
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import { i18n } from '$lib/i18n/i18n';
+	import Side from '$lib/components/service/side.svelte';
+	import Footer from '$lib/components/service/footer.svelte';
 
 	import '../app.css';
 </script>
 
 <ParaglideJS {i18n}>
-	<slot></slot>
+	<div class="flex min-h-screen flex-col px-4 pt-12 md:px-8">
+		<div class="mx-auto mb-16 w-full max-w-[90rem] flex-1 gap-12 sm:flex lg:gap-20">
+			<div class="w-48">
+				<Side />
+			</div>
+			<main class="flex-1">
+				<slot></slot>
+			</main>
+		</div>
+		<Footer />
+	</div>
 </ParaglideJS>
-
-<style></style>
--- service-frontend/src/routes/+page.svelte
+++ service-frontend/src/routes/+page.svelte
@@ -1,24 +1,18 @@
 <script>
 	import { SignIn, SignOut } from '@auth/sveltekit/components';
-	import Footer from '$lib/components/service/footer.svelte';
 </script>
 
 <svelte:head>
 	<title>ShortBook</title>
 </svelte:head>
 
+<p class="mb-4">Top</p>
+<a href="/mypage" class="mb-4">My page</a>
 <div>
-	<main>
-		<p class="mb-4">Top</p>
-		<a href="/mypage" class="mb-4">My page</a>
-		<div>
-			<SignIn>
-				<span slot="submitButton" class="pa-2 bg-primary-200">Sign in</span>
-			</SignIn>
-			<SignOut>
-				<span slot="submitButton" class="pa-2 bg-primary-200">Signout</span>
-			</SignOut>
-		</div>
-	</main>
-	<Footer />
+	<SignIn>
+		<span slot="submitButton" class="pa-2 bg-primary-200">Sign in</span>
+	</SignIn>
+	<SignOut>
+		<span slot="submitButton" class="pa-2 bg-primary-200">Signout</span>
+	</SignOut>
 </div>
