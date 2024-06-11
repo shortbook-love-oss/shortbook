5ee9838161cac622171e7a259c475a827e3d5843
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-11 16:51:32 +0900

    Create sidebar component

---
+++ lib-frontend/components/service/side-link.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let name = '';
+	export let href = '/';
+</script>
+
+<a
+	{href}
+	class="flex items-center rounded-lg p-3 text-stone-950 hover:bg-stone-200 focus:bg-stone-200"
+>
+	<div class="mr-2"><slot></slot></div>
+	<h2 class="flex-1 text-xl">{name}</h2>
+</a>
---
+++ lib-frontend/components/service/side.svelte
@@ -0,0 +1,37 @@
+<script>
+	import IconHome from '~icons/mdi/home-outline';
+	import IconSearch from '~icons/mdi/search';
+	import IconSetting from '~icons/mdi/settings-outline';
+	import IconSupport from '~icons/mdi/help-circle-outline';
+	import IconUser from '~icons/mdi/user';
+	import SideLink from './side-link.svelte';
+</script>
+
+<header>
+	<div class="mb-4 mb-6 px-4">
+		<img
+			src="/assets/shortbook-logotype.svg"
+			class="m-auto mb-2 aspect-logotype w-full max-w-60"
+			alt="Short book logo"
+		/>
+	</div>
+	<nav>
+		<ul>
+			<li>
+				<SideLink name="Home" href="/"><IconHome width="28" height="28" /></SideLink>
+			</li>
+			<li>
+				<SideLink name="Search" href="/"><IconSearch width="28" height="28" /></SideLink>
+			</li>
+			<li>
+				<SideLink name="Setting" href="/mypage"><IconSetting width="28" height="28" /></SideLink>
+			</li>
+			<li>
+				<SideLink name="Support" href="/"><IconSupport width="28" height="28" /></SideLink>
+			</li>
+			<li>
+				<SideLink name="Profile" href="/"><IconUser width="28" height="28" /></SideLink>
+			</li>
+		</ul>
+	</nav>
+</header>
