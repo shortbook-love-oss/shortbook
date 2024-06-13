020eca281b4a4ecab06959c441f5d3e1177d01e6
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-13 23:43:43 +0900

    Revert "Returns minimal sign-in user info"
    This reverts commit 420cab9e59cfd529186a091aecda3272267a91cb.

--- service-frontend/src/auth.ts
+++ service-frontend/src/auth.ts
@@ -9,18 +9,6 @@ const prisma = new PrismaClient();
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	trustHost: true,
 	adapter: PrismaAdapter(prisma),
-	secret: process.env.AUTH_SECRET, // Include env to build directory
 	providers: [LinkedIn, GitHub],
-	callbacks: {
-		async session({ session, user }: any) {
-			// Filter only using properties
-			session = {};
-			// Send properties to the client, like an access_token and user id from a provider
-			session.id = user.id;
-			session.email = user.email;
-			session.name = user.name;
-			session.image = user.image;
-			return session;
-		}
-	}
+	secret: process.env.AUTH_SECRET // Include env to build directory
 });
