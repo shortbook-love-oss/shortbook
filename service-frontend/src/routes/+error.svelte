e8404832b9e14038150dced4917dcbe9dedde9bf
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-14 23:58:53 +0900

    Add "Back to home" link to error page

--- service-frontend/src/routes/+error.svelte
+++ service-frontend/src/routes/+error.svelte
@@ -1,5 +1,7 @@
 <script>
+	import IconArrowLeft from '~icons/mdi/arrow-left';
 	import { page } from '$app/stores';
+	import NavLinkSmall from '$lib/components/service/navigation/nav-link-small.svelte';
 </script>
 
 <svelte:head>
@@ -8,9 +10,16 @@
 </svelte:head>
 
 <div class="flex h-screen flex-col items-center justify-center">
-	<div class="max-w-60 mb-8">
-		<img src="/assets/shortbook-logotype.svg" class="aspect-logotype w-full" alt="Short book logo" />
+	<div class="mb-8 max-w-60">
+		<img
+			src="/assets/shortbook-logotype.svg"
+			class="aspect-logotype w-full"
+			alt="Short book logo"
+		/>
 	</div>
 	<p class="text-2xl">{$page.status}</p>
-	<p class="text-lg">{$page.error?.message}</p>
+	<p class="mb-8 text-lg">{$page.error?.message}</p>
+	<NavLinkSmall name="Back to home" href="/">
+		<IconArrowLeft width="24" height="24" class="-mx-1" />
+	</NavLinkSmall>
 </div>
