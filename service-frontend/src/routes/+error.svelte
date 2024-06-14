5a1755496e0e340c06f0afd105839966762fe4a2
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-14 23:34:27 +0900

    Create error page minimum

---
+++ service-frontend/src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>{$page.status} {$page.error?.message} | ShortBook</title>
+	<meta name="robots" content="noindex" />
+</svelte:head>
+
+<div class="flex h-screen flex-col items-center justify-center">
+	<div class="max-w-60 mb-8">
+		<img src="/assets/shortbook-logotype.svg" class="aspect-logotype w-full" alt="Short book logo" />
+	</div>
+	<p class="text-2xl">{$page.status}</p>
+	<p class="text-lg">{$page.error?.message}</p>
+</div>
