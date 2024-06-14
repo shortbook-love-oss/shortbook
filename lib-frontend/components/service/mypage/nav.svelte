eb293981b81158d3104d587c613df38d983ec605
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-15 00:04:13 +0900

    Mypage nav only for auth user

--- lib-frontend/components/service/mypage/nav.svelte
+++ lib-frontend/components/service/mypage/nav.svelte
@@ -1,25 +1,9 @@
 <script>
-	import { SignIn } from '@auth/sveltekit/components';
-	import IconArrowRight from '~icons/mdi/arrow-right-thick';
-	import { page } from '$app/stores';
 	import NavLinkSmall from '../navigation/nav-link-small.svelte';
 	import { categories } from './menu';
 </script>
 
 <ul>
-	{#if !$page.data.session?.user}
-		<li class="mt-6">
-			<SignIn className="w-fit rounded-xl hover:invert focus-within:invert bg-primary-800 min-h-64">
-				<div
-					slot="submitButton"
-					class="px-4 py-3 text-start text-5xl leading-tight text-primary-50"
-				>
-					<p>Sign in and enjoy</p>
-					<IconArrowRight width="64" height="64" class="ml-auto" />
-				</div>
-			</SignIn>
-		</li>
-	{/if}
 	{#each categories as category (category.href)}
 		<li class="mt-6 font-semibold">
 			<p class="px-3 py-2 text-lg">{category.name}</p>
