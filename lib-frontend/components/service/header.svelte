0c982b8e86829700269ec96a575ee0bb8eaa3e9c
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-15 14:10:57 +0900

    Prettier header

--- lib-frontend/components/service/header.svelte
+++ lib-frontend/components/service/header.svelte
@@ -2,9 +2,12 @@
 	import Nav from '$lib/components/service/navigation/nav.svelte';
 </script>
 
-<header class="border-b border-stone-300 px-4">
-	<div class="flex items-center justify-between gap-4 m-auto max-w-[90rem]">
-		<a href="/" class="block shrink-0 rounded-md p-2 hover:bg-stone-200 focus:bg-stone-200">
+<header class="border-b border-stone-300 px-4 md:px-8 py-1">
+	<div class="m-auto flex max-w-[90rem] justify-between gap-4">
+		<a
+			href="/"
+			class="flex shrink-0 items-center rounded-md p-2 hover:bg-stone-200 focus:bg-stone-200"
+		>
 			<img
 				src="/assets/shortbook-logotype.svg"
 				class="aspect-logotype w-full max-w-36"
