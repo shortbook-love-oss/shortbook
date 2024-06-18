4725198b6a9d995db8d969360cd8971b3d494a6c
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-18 14:43:27 +0900

    Going back and forth between Sign-in/out

--- service-frontend/src/routes/(auth)/signin/+page.svelte
+++ service-frontend/src/routes/(auth)/signin/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import SignInByOAuth from '$lib/components/service/auth/signin-by-oauth.svelte';
+	import NavLinkSmall from '$lib/components/service/navigation/nav-link-small.svelte';
+
+	// After sign-in/sign-up redirect to
+	let searchParam = $page.url.search;
 </script>
 
-<h1 class="mb-12 text-center text-4xl font-semibold">Sign in</h1>
-<div class="mx-auto inline-block">
+<h1 class="mb-4 text-center text-4xl font-semibold">Sign in</h1>
+<div class="mx-auto mb-12 w-fit">
 	<SignInByOAuth providerName="LinkedIn" className="mb-4" />
 	<SignInByOAuth providerName="GitHub" />
+</div>
+<div class="text-center text-xl">
+	<p class="mb-2">Don't have an account yet?</p>
+	<NavLinkSmall
+		name="Sign up"
+		href="/signup{searchParam}"
+		className="mx-auto w-fit text-2xl"
+	/>
 </div>
--- service-frontend/src/routes/(auth)/signup/+page.svelte
+++ service-frontend/src/routes/(auth)/signup/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import SignInByOAuth from '$lib/components/service/auth/signin-by-oauth.svelte';
+	import NavLinkSmall from '$lib/components/service/navigation/nav-link-small.svelte';
+
+	// After sign-in/sign-up redirect to
+	let searchParam = $page.url.search;
 </script>
 
 <h1 class="mb-2 text-center text-4xl font-semibold">Sign up</h1>
@@ -10,9 +15,15 @@
 	and
 	<a href="/policies/privacy" class="underline">Privacy policy</a>.
 </p>
-<div class="flex flex-col">
-	<div class="mx-auto inline-block">
-		<SignInByOAuth isSignUp providerName="LinkedIn" className="mb-4" />
-		<SignInByOAuth isSignUp providerName="GitHub" />
-	</div>
+<div class="mx-auto mb-12 w-fit">
+	<SignInByOAuth isSignUp providerName="LinkedIn" className="mb-4" />
+	<SignInByOAuth isSignUp providerName="GitHub" />
+</div>
+<div class="text-center text-xl">
+	<p class="mb-2">Already have an account?</p>
+	<NavLinkSmall
+		name="Sign in"
+		href="/signin{searchParam}"
+		className="mx-auto w-fit text-2xl"
+	/>
 </div>
