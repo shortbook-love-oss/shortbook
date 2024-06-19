d5eaa6066b758c55cc9e7960c878e9f1c7f55bb0
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-19 17:34:46 +0900

    Submit button set to unsubmittable while submit loading

--- lib-frontend/components/modules/form/submit-button.svelte
+++ lib-frontend/components/modules/form/submit-button.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	export let className = '';
+	export let isLoading = false;
 </script>
 
-<button class="rounded-lg text-xl border-2 border-primary-700 bg-primary-100 text-primary-950 px-4 py-3 {className}">
+<button
+	class="rounded-lg border-2 border-primary-700 bg-primary-100 px-4 py-3 text-xl text-primary-950 {isLoading
+		? 'cursor-wait border-stone-400 bg-stone-100 text-stone-500'
+		: ''} {className}"
+	type={isLoading ? 'button' : 'submit'}
+	aria-disabled={isLoading}
+>
 	<slot />
 </button>
