d08ba333802e336ed703a06ac37c5ebab9e87d77
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-19 11:16:01 +0900

    Create Submit button component

---
+++ lib-frontend/components/modules/form/submit-button.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let className = '';
+</script>
+
+<button class="rounded-lg text-xl border-2 border-primary-700 bg-primary-100 text-primary-950 px-4 py-3 {className}">
+	<slot />
+</button>
