d1fbb45a0b25951d409902ac9afef36dbd0d5c20
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-20 23:45:18 +0900

    Show warning to form component

--- lib-frontend/components/modules/form/form.svelte
+++ lib-frontend/components/modules/form/form.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import IconCheck from '~icons/mdi/check';
+	import IconWarning from '~icons/mdi/warning';
 	import IconError from '~icons/mdi/warning-circle';
 	import SubmitButton from './submit-button.svelte';
 
@@ -8,6 +9,7 @@
 	export let hasInvalid = false;
 	export let isLoading = false;
 	export let submitLabel = 'Save';
+	export let warnMessage = '';
 	export let successMessage = '';
 	export let errorMessage = '';
 
@@ -18,10 +20,20 @@
 </script>
 
 <form use:enhance {...$$restProps}>
+	{#if warnMessage}
+		<div
+			class="mb-6 flex items-center gap-2 rounded-lg border-2 border-amber-600 bg-amber-100 p-4 text-amber-950"
+		>
+			<IconWarning width="24" height="24" class="shrink-0" />
+			<p class="text-lg leading-snug">
+				{warnMessage}
+			</p>
+		</div>
+	{/if}
 	{#if successMessage}
 		<noscript>
 			<div
-				class="mb-6 flex items-center gap-1 rounded-lg border-2 border-emerald-600 bg-emerald-50 p-4 text-emerald-950"
+				class="mb-6 flex items-center gap-2 rounded-lg border-2 border-emerald-600 bg-emerald-50 p-4 text-emerald-950"
 			>
 				<IconCheck width="24" height="24" class="shrink-0" />
 				<p class="text-lg leading-snug">
@@ -32,7 +44,7 @@
 	{/if}
 	{#if errorMessage}
 		<div
-			class="mb-6 flex items-center gap-1 rounded-lg border-2 border-primary-700 bg-red-100 p-4 text-red-900"
+			class="mb-6 flex items-center gap-2 rounded-lg border-2 border-red-700 bg-red-100 p-4 text-red-900"
 		>
 			<IconError width="24" height="24" class="shrink-0" />
 			<p class="text-lg leading-snug">
