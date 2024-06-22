3f95d975b46313c00042ff39673c790046eaa911
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-22 16:44:13 +0900

    Profile characters not allow invisible only

--- lib-frontend/validation/schema/profile-update.ts
+++ lib-frontend/validation/schema/profile-update.ts
@@ -1,14 +1,27 @@
 import { z } from 'zod';
 import { isLanguageTag } from '$lib/utilities/language';
+import { validateOnlyVisibleChar } from '$lib/utilities/validate';
 
 export const schema = z.object({
 	slug: z
 		.string()
-		.min(1)
 		.max(30)
-		.regex(/^[\w-.]+$/),
-	nativeLang: z.string().min(1).max(5).refine(isLanguageTag),
-	penName: z.string().min(1).max(50),
-	headline: z.string().max(50),
-	selfIntro: z.string().max(4e6) // less than 16,777,216 / 4 - 1
+		.regex(/[a-zA-Z0-9]/, {
+			message: 'Please include at least one alphanumeric character'
+		})
+		.regex(/^[\w-.]*$/, {
+			message: 'Use only alphanumeric, hyphens, underscore, and periods'
+		}),
+	nativeLang: z.string().max(5).refine(isLanguageTag, {
+		message: 'Please select language'
+	}),
+	penName: z.string().min(1).max(50).refine(validateOnlyVisibleChar, {
+		message: 'Cannot register using only invisible characters'
+	}),
+	headline: z.string().max(50).refine(validateOnlyVisibleChar, {
+		message: 'Cannot register using only invisible characters'
+	}),
+	selfIntro: z.string().max(4e6).refine(validateOnlyVisibleChar, {
+		message: 'Cannot register using only invisible characters'
+	}) // less than 16,777,216 / 4 - 1
 });
