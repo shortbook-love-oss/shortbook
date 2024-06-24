6a276f391021d5ade3c9f8bb1529fe4d8431623e
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-24 14:32:24 +0900

    If never signed-in device, redirect to sign-up page.

--- service-frontend/src/routes/mypage/+layout.server.ts
+++ service-frontend/src/routes/mypage/+layout.server.ts
@@ -1,12 +1,18 @@
-import { getLangTagPathPart } from '$lib/utilities/url.js';
 import { redirect } from '@sveltejs/kit';
+import { getUserId } from '$lib/utilities/cookie';
+import { getLangTagPathPart } from '$lib/utilities/url';
 
-export const load = async ({ locals, url }) => {
+export const load = async ({ locals, url, cookies }) => {
 	const session = await locals.auth();
 
 	if (!session?.user) {
-		// '/zh-cn/mypage' → '/de/signin?callbackUrl=https%3A%2F%2Fshortbook.life%2Fde%2Fmypage'
-		const redirectTo = new URL(`${url.origin}${getLangTagPathPart(url.pathname)}/signin`);
+		// '/zh-cn/mypage' → '/de/signup?callbackUrl=https%3A%2F%2Fshortbook.life%2Fde%2Fmypage'
+		let redirectToPathname = '/signup';
+		if (getUserId(cookies)) {
+			// "user-id" is set for devices where you have signed in
+			redirectToPathname = '/signin';
+		}
+		const redirectTo = new URL(url.origin + getLangTagPathPart(url.pathname) + redirectToPathname);
 		redirectTo.searchParams.set('callbackUrl', url.href);
 		throw redirect(303, redirectTo.href);
 	}
