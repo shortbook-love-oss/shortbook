a9820c9c81b975fd924a95707fed00c971bcf702
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-24 10:41:59 +0900

    Support dialog keyboard focusing

--- lib-frontend/components/layouts/dialog.svelte
+++ lib-frontend/components/layouts/dialog.svelte
@@ -1,73 +1,66 @@
 <script lang="ts">
-	import { clickoutside } from '@svelte-put/clickoutside';
+	import { onMount } from 'svelte';
 	import IconClose from '~icons/mdi/close';
+	import { onNavigate } from '$app/navigation';
 
 	// Need for unique attribute value
 	export let name: string;
+	export let openerClass = '';
 
-	// Open dialog by keyboard focus
-	function openDialog(e: Event) {
-		const openSwitch = document.getElementById('common_dialog_open_' + name) as HTMLInputElement;
-		requestAnimationFrame(() => {
-			openSwitch.checked = true;
-		});
-	}
+	let isEnableJS = false;
+	onMount(() => {
+		isEnableJS = true;
+	});
 
-	// Close dialog if open
-	const outsideClickThreshold = 50;
-	let outsideClickTimeout = 0;
+	onNavigate(() => {
+		closeDialog();
+	});
+
 	function closeDialog() {
-		const closeSwitch = document.getElementById('common_dialog_close_' + name) as HTMLInputElement;
-		if (outsideClickTimeout === 0) {
-			closeSwitch.checked = true;
-		}
-		// Anti-chattering measures
-		outsideClickTimeout = window.setTimeout(() => {
-			outsideClickTimeout = 0;
-		}, outsideClickThreshold);
+		const openSwitch = document.getElementById('common_dialog_open_' + name) as HTMLInputElement;
+		openSwitch.checked = false;
 	}
 </script>
 
-<button
-	type="button"
-	class="peer/common_dialog_open w-full rounded-lg text-start hover:bg-stone-200 focus:bg-stone-200"
-	on:click|self={openDialog}
->
-	<label for="common_dialog_open_{name}">
+<label class="peer/common_dialog_open relative">
+	<div class="focus-within:bg-stone-200 hover:bg-stone-200 {openerClass}">
 		<slot name="opener" />
-	</label>
-	<input type="radio" name="common_dialog_{name}" id="common_dialog_open_{name}" class="hidden" />
-</button>
+		<input
+			type="checkbox"
+			name="common_dialog_{name}"
+			id="common_dialog_open_{name}"
+			class="absolute left-0 top-0 h-full w-full opacity-0"
+		/>
+	</div>
+</label>
 
 <!-- Dialog -->
 <div
 	id="common_dialog_{name}"
-	class="invisible fixed left-[999%] top-0 z-50 flex h-dvh h-screen w-screen items-center justify-center bg-stone-400/30 p-4 peer-has-[:checked]/common_dialog_open:visible peer-has-[:checked]/common_dialog_open:left-0"
+	class="fixed left-0 top-0 z-50 flex hidden h-dvh h-screen w-screen items-center justify-center bg-stone-400/30 p-4 peer-has-[:checked]/common_dialog_open:flex"
 >
 	<div
 		class="inline-flex max-h-full max-w-2xl flex-col rounded-xl border-2 border-primary-300 bg-white"
-		use:clickoutside
-		on:clickoutside={closeDialog}
 	>
-		<div class="flex justify-end pb-1">
-			<label class="inline-block">
-				<slot name="closer">
-					<div class="leading-none">
-						<IconClose
-							width="44"
-							height="44"
-							class="ml-auto rounded-es rounded-se-[0.625rem] p-1 hover:bg-stone-200 focus:bg-stone-200"
-							aria-label="Cancel and close dialog"
-						/>
-					</div>
-				</slot>
-				<input
-					type="radio"
-					name="common_dialog_{name}"
-					checked
-					id="common_dialog_close_{name}"
-					class="hidden"
+		<div
+			class="relative ms-auto inline-flex leading-none focus-within:bg-stone-200 hover:bg-stone-200"
+		>
+			{#if isEnableJS}
+				<button
+					type="button"
+					class="absolute left-0 top-0 h-full w-full opacity-0"
+					on:click|self={closeDialog}
 				/>
+			{/if}
+			<label for="common_dialog_open_{name}" class="inline-block">
+				<slot name="closer">
+					<IconClose
+						width="44"
+						height="44"
+						class="ml-auto rounded-es rounded-se-[0.625rem] p-1"
+						aria-label="Cancel and close dialog"
+					/>
+				</slot>
 			</label>
 		</div>
 		<div class="overflow-x-auto px-4 py-1 sm:pr-6 md:pl-6 md:pr-8">
