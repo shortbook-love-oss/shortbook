30cecb265131dffc38d9353b5acb9894f50abde5
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-24 14:13:36 +0900

    If require sign-in, redirect to sign-in page instead of error page

--- service-frontend/src/routes/+error.svelte
+++ service-frontend/src/routes/+error.svelte
@@ -3,9 +3,6 @@
 	import { page } from '$app/stores';
 	import NavLinkSmall from '$lib/components/service/navigation/nav-link-small.svelte';
 	import LayoutRule from './_layout-rule.svelte';
-
-	// After sign-in/sign-up redirect to
-	let redirectUrl = encodeURIComponent($page.url.href);
 </script>
 
 <svelte:head>
@@ -17,23 +14,8 @@
 	<div slot="contents" class="flex h-full min-h-80 flex-col items-center justify-center p-8">
 		<p class="text-4xl">{$page.status}</p>
 		<p class="mb-8 text-xl">{$page.error?.message}</p>
-		{#if $page.status === 401}
-			<div class="flex gap-4">
-				<NavLinkSmall
-					name="Sign in"
-					href="/signin?callbackUrl={redirectUrl}"
-					className="border-2 border-primary-700 text-2xl text-red-800"
-				/>
-				<NavLinkSmall
-					name="Sign up"
-					href="/signup?callbackUrl={redirectUrl}"
-					className="border-2 border-primary-700 text-2xl text-red-800"
-				/>
-			</div>
-		{:else}
-			<NavLinkSmall name="Back to home" href="/">
-				<IconArrowLeft width="24" height="24" class="me-1" />
-			</NavLinkSmall>
-		{/if}
+		<NavLinkSmall name="Back to home" href="/">
+			<IconArrowLeft width="24" height="24" class="me-1" />
+		</NavLinkSmall>
 	</div>
 </LayoutRule>
--- service-frontend/src/routes/mypage/+layout.server.ts
+++ service-frontend/src/routes/mypage/+layout.server.ts
@@ -1,10 +1,14 @@
-import { error } from '@sveltejs/kit';
+import { getLangTagPathPart } from '$lib/utilities/url.js';
+import { redirect } from '@sveltejs/kit';
 
-export const load = async ({ locals }) => {
+export const load = async ({ locals, url }) => {
 	const session = await locals.auth();
 
 	if (!session?.user) {
-		throw error(401, { message: 'Unauthorized' });
+		// '/zh-cn/mypage' → '/de/signin?callbackUrl=https%3A%2F%2Fshortbook.life%2Fde%2Fmypage'
+		const redirectTo = new URL(`${url.origin}${getLangTagPathPart(url.pathname)}/signin`);
+		redirectTo.searchParams.set('callbackUrl', url.href);
+		throw redirect(303, redirectTo.href);
 	}
 
 	return {
