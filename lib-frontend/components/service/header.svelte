3e6ed830ee4f8382d8f93b2cee187c2deb22a945
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-06-27 16:31:59 +0900

    Align header content width and main content width

--- lib-frontend/components/service/header.svelte
+++ lib-frontend/components/service/header.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <header class="border-b border-stone-300">
-	<div class="m-auto flex w-full max-w-[90rem] justify-between gap-4 px-4 py-1 sm:px-6 md:px-8">
+	<div class="m-auto flex w-full max-w-[90rem] justify-between gap-4 px-4 py-1 md:px-6">
 		<a
 			href="/"
-			class="flex shrink-0 items-center rounded-md p-2 hover:bg-stone-200 focus:bg-stone-200"
+			class="flex shrink-0 items-center rounded-md px-3 py-2 hover:bg-stone-200 focus:bg-stone-200"
 		>
 			<img
 				src="/assets/shortbook-logotype.svg"
