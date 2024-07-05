b15ff8e3447fcbc03bab552c707d92f2c376b6f4
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-05 15:31:24 +0900

    Rewrite heading of book list for everyone

--- service-frontend/src/routes/(main)/+page.svelte
+++ service-frontend/src/routes/(main)/+page.svelte
@@ -6,11 +6,13 @@
 </script>
 
 <svelte:head>
-	<title>ShortBook — Publish platform</title>
+	<title>ShortBook — Writing platform</title>
 </svelte:head>
 
 <div class="mb-8 flex flex-wrap items-center gap-4">
-	<h1 class="text-2xl font-semibold">Recent published books</h1>
+	{#if data.bookList.length}
+		<h1 class="text-2xl font-semibold">Explore books</h1>
+	{/if}
 	<a
 		href="/write/new"
 		class="flex items-center gap-2 rounded-lg border-2 border-primary-700 bg-primary-100 px-3 py-2 text-lg font-semibold text-primary-950 hover:bg-primary-200"
