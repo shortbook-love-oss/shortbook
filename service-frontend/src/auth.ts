3cac2318d83050e5533633a0a133722d481954bf
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-05 11:30:37 +0900

    In production, secret keys for auth must be specified explicitly.

--- service-frontend/src/auth.ts
+++ service-frontend/src/auth.ts
@@ -1,16 +1,31 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import Google from "@auth/sveltekit/providers/google";
+import Google from '@auth/sveltekit/providers/google';
 import LinkedIn from '@auth/sveltekit/providers/linkedin';
 import GitHub from '@auth/sveltekit/providers/github';
 import { dbUserProfileCreate } from '$lib/model/user/profile/create';
 import { dbUserEmailUpdate } from '$lib/model/user/update-email';
 import prisma from '$lib/prisma/connect';
+import { env } from '$env/dynamic/private';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
+	secret: env.AUTH_SECRET,
 	trustHost: true,
 	adapter: PrismaAdapter(prisma),
-	providers: [Google, LinkedIn, GitHub],
+	providers: [
+		Google({
+			clientId: env.AUTH_GOOGLE_ID,
+			clientSecret: env.AUTH_GOOGLE_SECRET
+		}),
+		LinkedIn({
+			clientId: env.AUTH_LINKEDIN_ID,
+			clientSecret: env.AUTH_LINKEDIN_SECRET
+		}),
+		GitHub({
+			clientId: env.AUTH_GITHUB_ID,
+			clientSecret: env.AUTH_GITHUB_SECRET
+		})
+	],
 	pages: {
 		signIn: '/signin',
 		newUser: '/signup'
