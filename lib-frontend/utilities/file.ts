608277d4b6612e6f24d5ac96826a0f2187a5ca65
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-09 23:19:01 +0900

    Create file input component

--- lib-frontend/utilities/file.ts
+++ lib-frontend/utilities/file.ts
@@ -1,1 +1,12 @@
+export interface FileInputErrorMessages {
+	[key: number]: string[] | undefined;
+	_errors?: undefined;
+}
+
+export interface SelectedFile {
+	file: File;
+	dataUrl: string;
+	key: number;
+}
+
 export const imageMIMEs = ['image/png', 'image/jpeg'];
