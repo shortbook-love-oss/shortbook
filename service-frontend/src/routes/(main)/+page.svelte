3edd1455b49ec26a7c4d615d3ece2734e2feefd9
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-14 11:14:40 +0900

    i18n top page

--- service-frontend/src/routes/(main)/+page.svelte
+++ service-frontend/src/routes/(main)/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import IconPlus from '~icons/mdi/plus';
+	import * as m from '$lib/i18n/paraglide/messages.js';
 	import BookItem from '$lib/components/service/read/book-item.svelte';
 
 	export let data;
@@ -11,14 +12,14 @@
 
 <div class="mb-8 flex flex-wrap items-center gap-4">
 	{#if data.bookList.length}
-		<h1 class="text-2xl font-semibold">Explore books</h1>
+		<h1 class="text-2xl font-semibold">{m.top_heading()}</h1>
 	{/if}
 	<a
 		href="/write/new"
 		class="flex items-center gap-2 rounded-lg border-2 border-primary-700 bg-primary-100 px-3 py-2 text-lg font-semibold text-primary-950 hover:bg-primary-200"
 	>
 		<IconPlus width="24" height="24" class="-mx-1 shrink-0" />
-		<p>Write new book</p>
+		<p>{m.top_write_cta()}</p>
 	</a>
 </div>
 {#if data.bookList.length}
