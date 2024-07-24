5d44eff601e1295c1612198e3fd59459d98dc643
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-24 17:50:05 +0900

    Show current image with multi-size at profile image edit page

--- service-frontend/src/routes/mypage/personnel/image/+page.svelte
+++ service-frontend/src/routes/mypage/personnel/image/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import ProfileCard from '$lib/components/service/mypage/profile-card.svelte';
 	import { removeLangTagFromPath } from '$lib/utilities/url';
 	import ProfileImageUpload from '$lib/components/service/mypage/profile-image-upload.svelte';
 
@@ -14,7 +13,23 @@
 </svelte:head>
 
 <h1 class="mb-4 text-2xl font-semibold">Change profile image</h1>
-<ProfileCard name={data.penName} imageSrc={user?.image ?? ''} className="mb-8" />
+<div class="mb-8 flex flex-wrap items-start gap-4">
+	<img
+		src="{user?.image}?w=128&h=128&fit=cover"
+		class="h-32 w-32 rounded-md bg-white"
+		alt="Profile icon"
+	/>
+	<img
+		src="{user?.image}?w=64&h=64&fit=cover"
+		class="h-16 w-16 rounded-md bg-white"
+		alt="Profile icon"
+	/>
+	<img
+		src="{user?.image}?w=32&h=32&fit=cover"
+		class="h-8 w-8 rounded-md bg-white"
+		alt="Profile icon"
+	/>
+</div>
 <ProfileImageUpload
 	formData={data.form}
 	actionUrl={removeLangTagFromPath($page.url.pathname)}
