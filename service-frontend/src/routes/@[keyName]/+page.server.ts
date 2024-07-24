64223504abe9ebb9aa0715ee740f942b7475bd05
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-24 12:46:21 +0900

    Create user profile page

---
+++ service-frontend/src/routes/@[keyName]/+page.server.ts
@@ -0,0 +1,59 @@
+import { error } from '@sveltejs/kit';
+import { dbBookList } from '$lib/model/book/list';
+import { getBookCover } from '$lib/utilities/book';
+import type { BookItem } from '$lib/utilities/book';
+import { guessNativeLangFromRequest } from '$lib/utilities/language';
+
+export const load = async ({ request, params }) => {
+	const { books, dbError: bookDbError } = await dbBookList({ userKeyName: params.keyName });
+	if (!books || bookDbError) {
+		return error(500, { message: bookDbError?.message ?? '' });
+	}
+	const requestLang = guessNativeLangFromRequest(request);
+
+	const bookList: BookItem[] = [];
+	for (const book of books) {
+		const profile = book.user?.profiles;
+		let profileLang = profile?.languages.find((lang) => lang.language_code === requestLang);
+		if (!profileLang && profile?.languages.length) {
+			profileLang = profile.languages[0];
+		}
+		let bookLang = book.languages.find((lang) => lang.language_code === requestLang);
+		if (!bookLang && book.languages.length) {
+			bookLang = book.languages[0];
+		}
+		if (!book.user || !profile || !profileLang || !book.cover || !bookLang) {
+			continue;
+		}
+		const bookCover = getBookCover({
+			title: bookLang.title,
+			subtitle: bookLang.subtitle,
+			baseColorStart: book.cover.base_color_start,
+			baseColorEnd: book.cover.base_color_end,
+			baseColorDirection: book.cover.base_color_direction,
+			titleFontSize: book.cover.title_font_size,
+			titleAlign: book.cover.title_align,
+			titleColor: book.cover.title_color,
+			subtitleFontSize: book.cover.subtitle_font_size,
+			subtitleAlign: book.cover.subtitle_align,
+			subtitleColor: book.cover.subtitle_color,
+			writerAlign: book.cover.writer_align,
+			writerColor: book.cover.writer_color
+		});
+		bookList.push({
+			...bookCover,
+			id: book.id,
+			userId: book.user_id,
+			status: book.status,
+			title: bookLang.title,
+			subtitle: bookLang.subtitle,
+			publishedAt: book.published_at,
+			updatedAt: book.updated_at,
+			keyName: profile.key_name,
+			penName: profileLang.pen_name,
+			image: book.user.image ?? ''
+		});
+	}
+
+	return { bookList, requestLang };
+};
