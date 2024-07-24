917a840bd136de2c75605b858dd75f6dd2c73527
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-24 21:57:52 +0900

    Profile card link apply to image and name only

--- lib-frontend/components/service/mypage/profile-card.svelte
+++ lib-frontend/components/service/mypage/profile-card.svelte
@@ -5,20 +5,27 @@
 	export let className = '';
 </script>
 
-<svelte:element
-	this={keyName ? 'a' : 'p'}
-	href={keyName ? `/@${keyName}` : undefined}
-	class="flex w-full items-center gap-3 {keyName ? 'hover:underline' : ''} {className}"
->
+<div class="flex w-full items-center gap-3 {className}">
 	{#if imageSrc}
-		<img
-			src="{imageSrc}?w=64&h=64&fit=cover"
-			class="h-16 w-16 rounded-md bg-white"
-			alt="Profile icon"
-		/>
+		<svelte:element
+			this={keyName ? 'a' : 'div'}
+			href={keyName ? `/@${keyName}` : undefined}
+			class="peer shrink-0"
+		>
+			<img
+				src="{imageSrc}?w=64&h=64&fit=cover"
+				class="h-16 w-16 rounded-md bg-white"
+				alt="Profile icon"
+			/>
+		</svelte:element>
 	{/if}
-	<div class="flex flex-col overflow-x-hidden break-words">
-		<p class="whitespace-pre-wrap text-xl leading-snug">{name}</p>
+	<div class="flex flex-col overflow-x-hidden break-words peer-hover:[&>a]:underline">
+		<svelte:element
+			this={keyName ? 'a' : 'p'}
+			href={keyName ? `/@${keyName}` : undefined}
+			class="whitespace-pre-wrap text-xl leading-snug {keyName ? 'hover:underline' : ''}"
+			>{name}</svelte:element
+		>
 		<slot />
 	</div>
-</svelte:element>
+</div>
