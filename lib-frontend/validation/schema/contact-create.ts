bf55f0e03fff70d9c89f492cb85b78b0f3a8be6c
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-28 00:28:34 +0900

    Prevent email doble validation error

--- lib-frontend/validation/schema/contact-create.ts
+++ lib-frontend/validation/schema/contact-create.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const schema = z.object({
 	categoryKeyName: z.string().min(1).max(31),
-	email: z.string().email().min(1),
+	email: z.string().email(),
 	description: z.string().min(1).max(4e6), // less than 16,777,216 / 4 - 1
 	files: z
 		.instanceof(File, { message: 'Please select a file' })
