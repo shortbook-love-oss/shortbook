9ca98d23c15a536e758a35939bc390d75fcace31
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-28 10:02:38 +0900

    Put policy links to sign-in page / Adjust margin

--- service-frontend/src/routes/(auth)/signin/+page.svelte
+++ service-frontend/src/routes/(auth)/signin/+page.svelte
@@ -9,13 +9,20 @@
 	let searchParam = $page.url.search;
 </script>
 
-<h1 class="mb-4 text-center text-4xl font-semibold">Sign in</h1>
-<div class="mx-auto mb-12 w-fit">
-	<SignInByOAuth callbackUrl={data.callbackUrl} providerName="Google" className="mb-4" />
-	<SignInByOAuth callbackUrl={data.callbackUrl} providerName="LinkedIn" className="mb-4" />
-	<SignInByOAuth callbackUrl={data.callbackUrl} providerName="GitHub" />
-</div>
-<div class="text-center text-xl">
-	<p class="mb-2">Don't have an account yet?</p>
-	<NavLinkSmall name="Sign up" href="/signup{searchParam}" className="mx-auto w-fit text-2xl" />
-</div>
+<section class="text-lg">
+	<h1 class="mb-2 text-center text-4xl font-semibold">Sign in</h1>
+	<p class="mb-2">If you don't have an account yet, please agree to these policies.</p>
+	<div class="mb-12 flex flex-wrap items-center gap-x-8 gap-y-4">
+		<a href="/policies/term" class="underline">Terms of use</a>
+		<a href="/policies/privacy" class="underline">Privacy policy</a>
+	</div>
+	<div class="mx-auto mb-12 w-fit">
+		<SignInByOAuth callbackUrl={data.callbackUrl} providerName="Google" className="mb-4" />
+		<SignInByOAuth callbackUrl={data.callbackUrl} providerName="LinkedIn" className="mb-4" />
+		<SignInByOAuth callbackUrl={data.callbackUrl} providerName="GitHub" />
+	</div>
+	<div class="text-center">
+		<p class="mb-2">Don't have an account yet?</p>
+		<NavLinkSmall name="Sign up" href="/signup{searchParam}" className="mx-auto w-fit text-2xl" />
+	</div>
+</section>
--- service-frontend/src/routes/(auth)/signup/+page.svelte
+++ service-frontend/src/routes/(auth)/signup/+page.svelte
@@ -9,20 +9,25 @@
 	const searchParam = $page.url.search;
 </script>
 
-<h1 class="mb-2 text-center text-4xl font-semibold">Sign up</h1>
-<p class="mb-12 text-xl">
-	By creating an account, you agree to the <a href="/policies/term" class="underline"
-		>Terms of use</a
-	>
-	and
-	<a href="/policies/privacy" class="underline">Privacy policy</a>.
-</p>
-<div class="mx-auto mb-12 w-fit">
-	<SignInByOAuth isSignUp callbackUrl={data.callbackUrl} providerName="Google" className="mb-4" />
-	<SignInByOAuth isSignUp callbackUrl={data.callbackUrl} providerName="LinkedIn" className="mb-4" />
-	<SignInByOAuth isSignUp callbackUrl={data.callbackUrl} providerName="GitHub" />
-</div>
-<div class="text-center text-xl">
-	<p class="mb-2">Already have an account?</p>
-	<NavLinkSmall name="Sign in" href="/signin{searchParam}" className="mx-auto w-fit text-2xl" />
-</div>
+<section class="text-lg">
+	<h1 class="mb-2 text-center text-4xl font-semibold">Sign up</h1>
+	<p class="mb-2">By creating an account, you agree to these policies.</p>
+	<div class="mb-12 flex flex-wrap items-center gap-x-8 gap-y-4">
+		<a href="/policies/term" class="underline">Terms of use</a>
+		<a href="/policies/privacy" class="underline">Privacy policy</a>
+	</div>
+	<div class="mx-auto mb-12 w-fit">
+		<SignInByOAuth isSignUp callbackUrl={data.callbackUrl} providerName="Google" className="mb-4" />
+		<SignInByOAuth
+			isSignUp
+			callbackUrl={data.callbackUrl}
+			providerName="LinkedIn"
+			className="mb-4"
+		/>
+		<SignInByOAuth isSignUp callbackUrl={data.callbackUrl} providerName="GitHub" />
+	</div>
+	<div class="text-center">
+		<p class="mb-2">Already have an account?</p>
+		<NavLinkSmall name="Sign in" href="/signin{searchParam}" className="mx-auto w-fit text-2xl" />
+	</div>
+</section>
