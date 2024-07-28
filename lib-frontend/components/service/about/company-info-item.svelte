258e544ace57f8322d2e724fee6ad8fbbbe7c74c
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-28 22:46:36 +0900

    CompanyInfoItem use slot instead of description prop for multiple description

--- lib-frontend/components/service/about/company-info-item.svelte
+++ lib-frontend/components/service/about/company-info-item.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
-	export let heading = '';
+	export let heading: string;
 	export let content = '';
-	export let description = '';
 </script>
 
-<section class="mx-auto mb-16 max-w-2xl px-4 sm:px-8">
-	<h2 class="border-primary-700 mb-4 border-s-8 ps-4 text-4xl font-bold">{heading}</h2>
-	<p class="mb-4 ps-6 text-2xl">{content}</p>
-	{#if description}
-		<p class="mb-4 ps-6">{description}</p>
+<section class="mx-auto mb-16 max-w-2xl px-4 text-xl sm:px-8">
+	<h2 class="mb-4 border-s-8 border-primary-700 ps-4 text-4xl font-bold">{heading}</h2>
+	{#if content}
+		<p class="mb-4 ps-6">{content}</p>
+	{/if}
+	{#if $$slots.default}
+		<div class="mb-4 ps-6">
+			<slot />
+		</div>
 	{/if}
 </section>
