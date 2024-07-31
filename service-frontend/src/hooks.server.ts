e3f26fd318ca5c5e5761d3cf6877c25459ad1f1c
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-07-31 15:50:26 +0900

    After signed-out or not sign-up yet, delete cookie key for sign-in user

--- service-frontend/src/hooks.server.ts
+++ service-frontend/src/hooks.server.ts
@@ -2,7 +2,7 @@ import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { i18n } from '$lib/i18n/i18n';
 import { setAuthUserId, getAuthUserId } from '$lib/utilities/server/cookie';
-import { getSessionToken } from '$lib/utilities/cookie';
+import { setOption, keyAuthUserId, getSessionToken } from '$lib/utilities/cookie';
 import { handle as handleAuth } from './auth';
 
 const handleUser: Handle = async function ({ event, resolve }) {
@@ -18,6 +18,8 @@ const handleUser: Handle = async function ({ event, resolve }) {
 		if (session?.user.id !== cookieUserId) {
 			setAuthUserId(event.cookies, session.user.id ?? '');
 		}
+	} else {
+		event.cookies.delete(keyAuthUserId, setOption);
 	}
 
 	return resolve(event);
