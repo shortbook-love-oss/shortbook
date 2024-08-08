a5e17d1c9c2765fc33b8d1eda4a1bb08a9bb412a
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-08-08 21:38:21 +0900

    Increment created_at/updated_at for sort

--- lib-frontend/model/book-buy/create.ts
+++ lib-frontend/model/book-buy/create.ts
@@ -11,6 +11,8 @@ export interface DbBookBuyCreateRequest {
 }
 
 export async function dbBookBuyCreate(req: DbBookBuyCreateRequest) {
+	// Increment created_at/updated_at for sort
+	const createdAt = new Date();
 	let dbError: Error | undefined;
 
 	await prisma
@@ -22,33 +24,43 @@ export async function dbBookBuyCreate(req: DbBookBuyCreateRequest) {
 						user_id: req.userId,
 						amount: req.beforePointChargeAmount,
 						book_id: '',
-						payment_session_id: req.paymentSessionId
+						payment_session_id: req.paymentSessionId,
+						created_at: createdAt,
+						updated_at: createdAt
 					}
 				});
+				createdAt.setMilliseconds(createdAt.getMilliseconds() + 1);
 			}
-			// Reader's point spend
-			await tx.user_points.create({
-				data: {
-					user_id: req.userId,
-					amount: -req.pointSpend,
-					book_id: req.bookId,
-					payment_session_id: ''
-				}
-			});
-			await tx.book_buys.create({
-				data: {
-					book_id: req.bookId,
-					user_id: req.userId,
-					point_spend: req.pointSpend
-				}
-			});
 			// Writer's point charge
 			await tx.user_points.create({
 				data: {
 					user_id: req.writeUserId,
 					amount: req.pointSpend,
 					book_id: req.bookId,
-					payment_session_id: ''
+					payment_session_id: '',
+					created_at: createdAt,
+					updated_at: createdAt
+				}
+			});
+			createdAt.setMilliseconds(createdAt.getMilliseconds() + 1);
+			// Reader's point spend
+			await tx.user_points.create({
+				data: {
+					user_id: req.userId,
+					amount: -req.pointSpend,
+					book_id: req.bookId,
+					payment_session_id: '',
+					created_at: createdAt,
+					updated_at: createdAt
+				}
+			});
+			await tx.book_buys.create({
+				data: {
+					book_id: req.bookId,
+					user_id: req.userId,
+					point_spend: req.pointSpend,
+					created_at: createdAt,
+					updated_at: createdAt
 				}
 			});
 		})
