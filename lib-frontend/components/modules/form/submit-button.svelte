ac946d405836c1c76eb7b36686b09cd3f7fee0a5
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-08-16 13:56:56 +0900

    SubmitText like the SubmitButton

--- lib-frontend/components/modules/form/submit-button.svelte
+++ lib-frontend/components/modules/form/submit-button.svelte
@@ -14,10 +14,10 @@
 </script>
 
 <button
-	class="flex items-center justify-center gap-2 rounded-lg border-2 border-primary-700 bg-primary-100 px-4 py-3 text-xl text-stone-950 {hasInvalid ||
+	class="flex items-center justify-center gap-2 rounded-lg border-2 border-primary-700 bg-primary-100 px-4 py-3 text-xl {hasInvalid ||
 	isLoading
-		? 'border-stone-400 bg-stone-100 text-stone-500'
-		: 'hover:bg-primary-200'} {cursorClass} {className}"
+		? 'border-stone-400 bg-stone-100 text-stone-700'
+		: 'text-stone-950 hover:bg-primary-200'} {cursorClass} {className}"
 	type={hasInvalid || isLoading ? 'button' : 'submit'}
 	aria-disabled={hasInvalid || isLoading ? true : undefined}
 	{...$$restProps}
--- lib-frontend/components/modules/form/submit-text.svelte
+++ lib-frontend/components/modules/form/submit-text.svelte
@@ -2,15 +2,22 @@
 	export let className = '';
 	export let hasInvalid = false;
 	export let isLoading = false;
+
+	$: cursorClass = (() => {
+		if (hasInvalid) {
+			return 'cursor-not-allowed';
+		} else if (isLoading) {
+			return 'cursor-wait';
+		}
+		return '';
+	})();
 </script>
 
 <button
-	class="flex items-center gap-2 rounded-lg px-4 py-[0.875rem] text-xl text-stone-950 {hasInvalid ||
+	class="flex items-center justify-center gap-2 rounded-lg px-4 py-[0.875rem] text-xl {hasInvalid ||
 	isLoading
-		? 'border-stone-400 bg-stone-100 text-stone-500'
-		: 'hover:bg-stone-500/30'} {hasInvalid ? 'cursor-not-allowed' : ''} {isLoading
-		? 'cursor-wait'
-		: ''} {className}"
+		? 'border-stone-400 bg-stone-200 text-stone-700'
+		: 'text-stone-950 hover:bg-stone-500/30'} {cursorClass} {className}"
 	type={hasInvalid || isLoading ? 'button' : 'submit'}
 	aria-disabled={hasInvalid || isLoading ? true : undefined}
 	{...$$restProps}
