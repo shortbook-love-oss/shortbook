7ccf4facaa8b5592975fca5f92953ca0cc6ecd7f
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-08-20 09:05:23 +0900

    Hide sp-nav in sign-in/up page

--- lib-frontend/components/service/navigation/nav-sp.svelte
+++ lib-frontend/components/service/navigation/nav-sp.svelte
@@ -7,22 +7,13 @@
 	import IconSignup from '~icons/mdi/register-outline';
 	import { page } from '$app/stores';
 	import * as m from '$lib/i18n/paraglide/messages';
-	import { callbackParam, removeLanguageTagFromPath } from '$lib/utilities/url';
+	import { callbackParam } from '$lib/utilities/url';
 	import Dropdown from '$lib/components/layouts/dropdown.svelte';
 	import Signout from '$lib/components/service/auth/signout.svelte';
 	import NavLinkSp from './nav-link-sp.svelte';
 
 	// After sign-in/up redirect to
-	$: redirectPathname = (() => {
-		if (['/signin', '/signup'].includes(removeLanguageTagFromPath($page.url.pathname))) {
-			// On sign-in/up → sign-in/up page move, keep callback url;
-			const callbackUrl = $page.url.searchParams.get(callbackParam) ?? '';
-			return encodeURIComponent(callbackUrl);
-		} else {
-			// On (any page) → sign-in/up page move, show the (any page) after sign-in/up
-			return encodeURIComponent($page.url.href);
-		}
-	})();
+	$: redirectPathname = encodeURIComponent($page.url.href);
 </script>
 
 <header
