4f4d47017e9599b66e6f8c823201625b1b5348b5
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-08-20 19:21:58 +0900

    Profile page font change to serif, for authority

--- service-frontend/src/routes/@[userKey]/+page.svelte
+++ service-frontend/src/routes/@[userKey]/+page.svelte
@@ -17,18 +17,18 @@
 		<section
 			class="flex min-h-[27rem] w-full flex-col items-center rounded-lg bg-gradient-to-b from-primary-800 to-primary-600 p-4 pb-5 text-white md:min-h-[30rem]"
 		>
-			<h1 class="mb-1 max-w-full whitespace-pre-wrap break-words text-2xl">
+			<h1 class="max-w-full whitespace-pre-wrap break-words text-3xl">
 				{data.profileLang?.pen_name}
 			</h1>
-			<p class="mb-3 max-w-full break-words">@{data.user.profiles?.key_name}</p>
+			<p class="mb-6 max-w-full break-words">@{data.user.profiles?.key_name}</p>
 			{#if data.profileLang?.headline}
-				<p class="mb-4 max-w-full whitespace-pre-wrap break-words text-lg">
+				<p class="mb-8 max-w-full whitespace-pre-wrap break-words text-lg">
 					{data.profileLang.headline}
 				</p>
 			{/if}
 			<img
 				src="{data.user.image}?w=128&h=128&fit=cover"
-				class="mb-4 h-32 w-32 rounded-md"
+				class="mb-4 h-32 w-32 rounded-md bg-white"
 				alt="{data.profileLang?.headline} profile image"
 			/>
 			{#if data.isOwn}
@@ -43,7 +43,7 @@
 		</section>
 	</div>
 	<div
-		class="w-full flex-1 overflow-x-hidden rounded-lg bg-gradient-to-b from-primary-800 to-primary-600 p-1 text-lg {data
+		class="w-full flex-1 overflow-x-hidden rounded-lg bg-gradient-to-b from-primary-800 to-primary-600 p-1 font-serif text-lg {data
 			.profileLang?.self_introduction
 			? ''
 			: 'hidden'}"
@@ -56,8 +56,8 @@
 	</div>
 </div>
 
-<div class="mx-auto max-w-[108rem]">
-	<h2 class="mb-8 text-2xl font-semibold">Recent books</h2>
+<div class="mx-auto max-w-2xl">
+	<h2 class="mb-8 text-2xl">Recent books</h2>
 	{#if data.bookList.length}
 		<ul>
 			{#each data.bookList as book (book.id)}
