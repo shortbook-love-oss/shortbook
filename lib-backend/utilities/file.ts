3a31bc4c061f91a7fffec497eb9806aa833a4102
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-09-06 11:15:24 +0900

    Create func that delete file or dir from S3

--- lib-backend/utilities/file.ts
+++ lib-backend/utilities/file.ts
@@ -1,4 +1,10 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import {
+	DeleteObjectsCommand,
+	GetObjectCommand,
+	ListObjectsV2Command,
+	PutObjectCommand,
+	S3Client
+} from '@aws-sdk/client-s3';
 import { env } from '$env/dynamic/private';
 
 export interface StorageBucket {
@@ -6,8 +12,8 @@ export interface StorageBucket {
 	storageCdnBucketName: string;
 }
 
-export async function getFile(region: string, bucketName: string, filePath: string) {
-	const s3 = new S3Client({
+function createClient(region: string) {
+	return new S3Client({
 		region,
 		forcePathStyle: true,
 		maxAttempts: 2,
@@ -16,6 +22,10 @@ export async function getFile(region: string, bucketName: string, filePath: stri
 			secretAccessKey: env.AWS_SECRET_ACCESS_KEY
 		}
 	});
+}
+
+export async function getFile(region: string, bucketName: string, filePath: string) {
+	const s3 = createClient(region);
 
 	let file: Uint8Array | undefined;
 	let contentType = '';
@@ -45,15 +55,7 @@ export async function uploadFile(
 	bucketName: string,
 	filePath: string
 ) {
-	const s3 = new S3Client({
-		region,
-		forcePathStyle: true,
-		maxAttempts: 2,
-		credentials: {
-			accessKeyId: env.AWS_ACCESS_KEY_ID,
-			secretAccessKey: env.AWS_SECRET_ACCESS_KEY
-		}
-	});
+	const s3 = createClient(region);
 
 	let isSuccessUpload = false;
 	let error;
@@ -67,12 +69,63 @@ export async function uploadFile(
 	});
 	await s3
 		.send(command)
-		.then((uploadResult) => {
-			isSuccessUpload = uploadResult.$metadata.httpStatusCode === 200;
+		.then((response) => {
+			isSuccessUpload = response.$metadata.httpStatusCode === 200;
 		})
 		.catch((e: Error) => {
 			error = e;
 		});
 
 	return { isSuccessUpload, error };
+}
+
+export async function deleteFiles(region: string, bucketName: string, filePrefix: string) {
+	const s3 = createClient(region);
+
+	let isSuccessDelete = false;
+	let error: Error | undefined;
+
+	// S3 isn't support directory-delete
+	// Search by prefix and delete matched objects
+	const listCommand = new ListObjectsV2Command({
+		Bucket: bucketName,
+		Prefix: filePrefix,
+	})
+	const list = await s3.send(listCommand)
+		.catch((e: Error) => {
+			error = e;
+			return undefined;
+		});
+	if (!list || error) {
+		return { isSuccessDelete: false, error };
+	}
+
+	if (list.Contents?.length) {
+		const objectKeys: string[] = [];
+		list.Contents.forEach((object) => {
+			if (object.Key) {
+				objectKeys.push(object.Key);
+			}
+		});
+
+		const deleteCommand = new DeleteObjectsCommand({
+			Bucket: bucketName,
+			Delete: {
+				Objects: objectKeys.map(key => ({ Key: key }))
+			}
+		});
+		await s3
+			.send(deleteCommand)
+			.then((response) => {
+				const statusCode = response.$metadata.httpStatusCode;
+				if (statusCode) {
+					isSuccessDelete = 200 <= statusCode && statusCode < 300;
+				}
+			})
+			.catch((e: Error) => {
+				error = e;
+			});
+	}
+
+	return { isSuccessDelete, error };
 }
