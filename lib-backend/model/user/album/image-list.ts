a20a374fcad7fa6ff005caba3c19899bb8e6d195
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-09-12 13:01:27 +0900

    Don't show soft-deleted album images

--- lib-backend/model/user/album/image-list.ts
+++ lib-backend/model/user/album/image-list.ts
@@ -3,19 +3,30 @@ import prisma from '$lib-backend/database/connect';
 export interface DbUserAlbumImageListRequest {
 	userId?: string;
 	languageCode: string;
+	isIncludeDelete?: boolean;
 }
 
 export async function dbUserAlbumImageList(req: DbUserAlbumImageListRequest) {
 	let dbError: Error | undefined;
 
+	const whereCondDelete: { deleted_at?: null } = {};
+	if (!req.isIncludeDelete) {
+		whereCondDelete.deleted_at = null;
+	}
+
 	const albumImages = await prisma.user_images
 		.findMany({
 			where: {
-				user_id: req.userId
+				user_id: req.userId,
+				...whereCondDelete
 			},
 			include: {
-				property: true,
-				tags: true
+				property: {
+					where: { ...whereCondDelete }
+				},
+				tags: {
+					where: { ...whereCondDelete }
+				}
 			},
 			orderBy: {
 				created_at: 'desc'
