0c4d76a8f439c62c9cd0d745db778c6f7d520f0c
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-09-13 09:27:47 +0900

    Create func that convert to byte-length with unit, e.g. 6789 → 6.63 KB

--- lib-frontend/utilities/file.ts
+++ lib-frontend/utilities/file.ts
@@ -19,3 +19,22 @@ export const imageMIMEextension: Record<string, string> = {
 
 export const maybeSvgMIMEs = ['image/svg+xml', 'application/xml', 'text/xml'];
 export const maybeIcoMIMEs = ['image/x-icon'];
+
+export function getUnitByteLength(byteLength: number, decimalPoint: number) {
+	if (!byteLength || decimalPoint < 0) {
+		return 'n/a';
+	}
+
+	const sizeUnits = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+	let sizeUnitIndex = 0;
+	for (let i = sizeUnits.length - 1; i > 0; i--) {
+		if (byteLength / 1024 ** i >= 1) {
+			sizeUnitIndex = i;
+			break;
+		}
+	}
+	const unitAmount = (byteLength / 1024 ** sizeUnitIndex).toFixed(sizeUnitIndex ? decimalPoint : 0);
+	const unitAmountFixed = unitAmount.replace(`.${'0'.repeat(decimalPoint)}`, '');
+
+	return `${unitAmountFixed} ${sizeUnits[sizeUnitIndex]}`;
+}
