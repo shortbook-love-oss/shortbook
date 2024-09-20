9eb07925183fec91e952946c717a62e326a25c89
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-09-20 17:12:16 +0900

    Get session user by custom code instead of Auth.js inner process

---
+++ lib-backend/model/user/get-by-session-token.ts
@@ -0,0 +1,30 @@
+import prisma from '$lib-backend/database/connect';
+
+export interface DbUserGetBySessionTokenRequest {
+	sessionToken: string;
+}
+
+export async function dbUserGetBySessionToken(req: DbUserGetBySessionTokenRequest) {
+	const now = new Date();
+	let dbError: Error | undefined;
+
+	const user = await prisma.users
+		.findFirst({
+			where: {
+				sessions: {
+					every: {
+						session_token: req.sessionToken,
+						expires: {
+							gte: now
+						}
+					}
+				}
+			}
+		})
+		.catch(() => {
+			dbError ??= new Error(`Failed to get user.`);
+			return undefined;
+		});
+
+	return { user, dbError };
+}
--- service-frontend/src/hooks.server.ts
+++ service-frontend/src/hooks.server.ts
@@ -2,16 +2,27 @@ import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { i18n } from '$lib/i18n/i18n';
 import { getSessionToken } from '$lib/utilities/cookie';
-import { handle as handleAuth } from './auth';
+import { dbUserGetBySessionToken } from '$lib-backend/model/user/get-by-session-token';
 
 const handleUser: Handle = async function ({ event, resolve }) {
-	let session = null;
-	if (getSessionToken(event.cookies)) {
-		session = await event.locals.auth();
+	event.locals.signInUser = null;
+
+	const sessionToken = getSessionToken(event.cookies);
+	if (sessionToken) {
+		const { user } = await dbUserGetBySessionToken({ sessionToken });
+		if (user) {
+			event.locals.signInUser = {
+				id: user.id,
+				keyHandle: user.key_handle,
+				name: user.name,
+				email: user.email,
+				imageSrc: user.image_src,
+				nativeLanguage: user.native_language
+			};
+		}
 	}
-	event.locals.session = session;
 
 	return resolve(event);
 };
 
-export const handle = sequence(i18n.handle(), handleAuth, handleUser);
+export const handle = sequence(i18n.handle(), handleUser);
