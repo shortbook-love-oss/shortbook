d492da8863eb15494fa235bf3d27fa43f4192972
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-09-23 14:06:01 +0900

    Set user currency when user create, decide by url lang-tag

--- lib-backend/model/user/create.ts
+++ lib-backend/model/user/create.ts
@@ -1,12 +1,15 @@
 import prisma from '$lib-backend/database/connect';
+import type { CurrencySupportCodes } from '$lib/utilities/currency';
+import type { AvailableLanguageTags } from '$lib/utilities/language';
 
 export interface DbUserCreateRequest {
 	keyHandle: string;
 	penName: string;
 	emailEncrypt: string;
 	emailHash: string;
-	nativeLanguage: string;
 	imageSrc: string;
+	nativeLanguage: AvailableLanguageTags;
+	currency: CurrencySupportCodes;
 }
 
 export async function dbUserCreate(req: DbUserCreateRequest) {
@@ -19,13 +22,18 @@ export async function dbUserCreate(req: DbUserCreateRequest) {
 				pen_name: req.penName,
 				email: req.emailEncrypt,
 				email_hash: req.emailHash,
-				native_language: req.nativeLanguage,
 				image_src: req.imageSrc,
+				native_language: req.nativeLanguage,
 				languages: {
 					create: {
 						target_language: '',
 						headline: '',
 						self_introduction: ''
+					}
+				},
+				payment_setting: {
+					create: {
+						currency: req.currency
 					}
 				}
 			}
