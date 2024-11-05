0ef5dc887966dd4dffc9cb0312a77951bbbe1fdd
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-11-05 23:27:58 +0900

    Validate book prologue and content is JSON

--- lib-frontend/validation/schema/book/draft-create.ts
+++ lib-frontend/validation/schema/book/draft-create.ts
@@ -1,17 +1,17 @@
 import { z } from 'zod';
-import { validateOnlyVisibleChar } from '$lib/validation/rules/string';
+import { validAsJson, validateOnlyVisibleChar } from '$lib/validation/rules/string';
 
 export const schema = z.object({
-	title: z.string().min(1).max(200).refine(validateOnlyVisibleChar, {
+	title: z.string().max(200).refine(validateOnlyVisibleChar, {
 		message: 'Cannot register using only invisible characters'
 	}),
 	subtitle: z.string().max(200).refine(validateOnlyVisibleChar, {
 		message: 'Cannot register using only invisible characters'
 	}),
-	prologue: z.string().min(1).max(5e8).refine(validateOnlyVisibleChar, {
-		message: 'Cannot register using only invisible characters'
+	prologue: z.string().min(1).max(5e8).refine(validAsJson, {
+		message: 'Invalid JSON'
 	}),
-	content: z.string().min(1).max(5e8).refine(validateOnlyVisibleChar, {
-		message: 'Cannot register using only invisible characters'
+	content: z.string().min(1).max(5e8).refine(validAsJson, {
+		message: 'Invalid JSON'
 	})
 });
