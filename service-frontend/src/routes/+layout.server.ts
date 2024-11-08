ffc234fa8b75030f52ca9882caa88167a7258210
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-11-08 11:35:11 +0900

    Prevent console warning of cookie "paraglide:lang", it will be invalid

--- service-frontend/src/routes/+layout.server.ts
+++ service-frontend/src/routes/+layout.server.ts
@@ -1,11 +1,13 @@
 import { setOption } from '$lib/utilities/cookie';
 
 export const load = async ({ cookies, locals }) => {
-	// Force empty paraglide:lang for best privacy
-	cookies.set('paraglide:lang', '', {
-		...setOption,
-		secure: false
-	});
+	if (process.env.NODE_ENV !== 'development') {
+		// Force empty paraglide:lang for best privacy
+		cookies.set('paraglide:lang', '', {
+			...setOption,
+			secure: false
+		});
+	}
 
 	return { signInUser: locals.signInUser };
 };
