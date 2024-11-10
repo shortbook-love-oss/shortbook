edd86029c59cd13da96f3ae81a0ac7d1c91ea968
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-11-10 23:16:40 +0900

    Add info-message component

---
+++ lib-frontend/components/modules/information/message-info.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import IconInfo from '~icons/mdi/information-slab-circle';
+
+	type Props = {
+		className?: string;
+	} & (
+		| {
+				children: Snippet;
+				message?: never;
+		  }
+		| {
+				children?: never;
+				message: string;
+		  }
+	);
+	let { children, message = '', className = '' }: Props = $props();
+</script>
+
+{#if children || message}
+	<div
+		class="flex items-center gap-2 rounded-lg border-2 border-blue-400 bg-blue-50 p-4 text-lg leading-snug text-blue-950 {className}"
+	>
+		<IconInfo width="24" height="24" class="shrink-0" />
+		{#if children}
+			<div>{@render children()}</div>
+		{:else}
+			<p>{message}</p>
+		{/if}
+	</div>
+{/if}
