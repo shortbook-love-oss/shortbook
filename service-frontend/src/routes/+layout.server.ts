94a4852b207b3646f6759f53a6021903d89bbcf2
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-11-13 16:17:53 +0900

    Don't use deprecated cookie key even if production

--- service-frontend/src/routes/+layout.server.ts
+++ service-frontend/src/routes/+layout.server.ts
@@ -1,13 +1,3 @@
-import { setOption } from '$lib/utilities/cookie';
-
-export const load = async ({ cookies, locals }) => {
-	if (process.env.NODE_ENV !== 'development') {
-		// Force empty paraglide:lang for best privacy
-		cookies.set('paraglide:lang', '', {
-			...setOption,
-			secure: false
-		});
-	}
-
+export const load = async ({ locals }) => {
 	return { signInUser: locals.signInUser };
 };
