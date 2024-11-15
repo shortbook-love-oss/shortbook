61d44d13287f358a3284b6384a1b6764170b2737
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2024-11-15 23:53:10 +0900

    Able to switch 13 languages by lang-selector

--- lib-frontend/utilities/language.ts
+++ lib-frontend/utilities/language.ts
@@ -9,18 +9,18 @@ export interface LanguageSelect extends SelectItemSingle<AvailableLanguageTags |
 
 export const languageSelect: LanguageSelect[] = [
 	{ value: 'en', label: 'English', english: 'English' },
-	{ value: 'ja', label: '日本語', english: 'Japanese' }
-	// { value: 'ar-ae', label: 'العربية (الإمارات العربية المتحدة)', english: 'Arabic (U.A.E.)' },
-	// { value: 'fr', label: 'Français', english: 'French' },
-	// { value: 'de', label: 'Deutsch', english: 'German' },
-	// { value: 'es', label: 'Español', english: 'Spanish' },
-	// { value: 'it', label: 'Italiano', english: 'Italian' },
-	// { value: 'pt-br', label: 'Português (Brasil)', english: 'Portuguese (Brazil)' },
-	// { value: 'hi', label: 'हिन्दी', english: 'Hindi' },
-	// { value: 'zh-cn', label: '简体字', english: 'Chinese (Beijing)' },
-	// { value: 'zh-tw', label: '繁體字', english: 'Chinese (Taiwan)' },
-	// { value: 'ko', label: '한국어', english: 'Korean' },
-	// { value: 'ru', label: 'Русский язык', english: 'Russian' }
+	{ value: 'ja', label: '日本語', english: 'Japanese' },
+	{ value: 'ar-ae', label: 'العربية (الإمارات العربية المتحدة)', english: 'Arabic (U.A.E.)' },
+	{ value: 'fr', label: 'Français', english: 'French' },
+	{ value: 'de', label: 'Deutsch', english: 'German' },
+	{ value: 'es', label: 'Español', english: 'Spanish' },
+	{ value: 'it', label: 'Italiano', english: 'Italian' },
+	{ value: 'pt-br', label: 'Português (Brasil)', english: 'Portuguese (Brazil)' },
+	{ value: 'hi', label: 'हिन्दी', english: 'Hindi' },
+	{ value: 'zh-cn', label: '简体字', english: 'Chinese (Beijing)' },
+	{ value: 'zh-tw', label: '繁體字', english: 'Chinese (Taiwan)' },
+	{ value: 'ko', label: '한국어', english: 'Korean' },
+	{ value: 'ru', label: 'Русский язык', english: 'Russian' }
 ];
 
 export const languageAndNotSelect: LanguageSelect[] = [
